ee0c3fddbdc3df018eb023bd5c7fc9f19e0fb647
mathildahansson <142892122+mathildahansson@users.noreply.github.com>
2024-02-02 20:17:25 +0100

    lagt till mer JavaScript, pusslat med div'ar och styleat lite CSS

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-const resultCard = document.querySelector("#result-card");
-
 fetch("marsrover.json")
-    .then((response) => { return response.json() })
-    .then((data) => {
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
 
-// Hämta information:
-const mars = data.results;
-const searchString = document.querySelector("#searchInput");
+        const marsRover = data.photos;
+        console.log(marsRover);
 
-const output = mars
-.filter(n => photos.name.toLowerCase().includes(searchString))
-.map(n => `<p>${photos.name}</p>`);
 
-document.querySelector("#result-card").innerHTML = output;
+
+        const output = marsRover.map(obj => `
+        <div id="box">
+
+        <div id="box-header">
+        <h2 id="rover-name">${obj.rover.name}</h2>
+        <p id="id"><strong>ID:</strong> ${obj.id}</p>
+        </div>
+        
+        <div id="box-img"><img src="${obj.img_src}" id="rover-photos"></div>
+
+        <div id="box-info">
+        <p><strong>Camera:</strong> ${obj.camera.full_name}</p>
+        <p><strong>Earth date:</strong> ${obj.earth_date}</p>
+        <p><strong>Sol:</strong> ${obj.sol}</p>
+        </div>
+
+        </div>`).join("");
+
+        document.querySelector("#container").innerHTML = output;
 
     });
