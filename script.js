b5cacf3fa2e2530a79ebcb4328d16e69c3678102
mathildahansson <142892122+mathildahansson@users.noreply.github.com>
2024-02-05 00:01:47 +0100

    försökt lösa en del javascript, ändrat API metod och lagt till lite html och ändrat lite css

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-fetch("marsrover.json")
+const apiKey = "11dDRgu72MePeDUUGFadfofpAf6Ccc3p45e1ekdf";
+const url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=11dDRgu72MePeDUUGFadfofpAf6Ccc3p45e1ekdf";
+
+let displayData = document.querySelector("#display-data");
+
+
+let button = document.querySelector("#searchButton");
+
+// button.addEventListener("click", => () {})
+
+
+
+
+ 
+    
+
+
+    fetch(apiKey)
     .then(response => response.json())
     .then(data => {
         console.log(data);
@@ -16,7 +33,7 @@ fetch("marsrover.json")
         <p id="id"><strong>ID:</strong> ${obj.id}</p>
         </div>
         
-        <div id="box-img"><img src="${obj.img_src}" id="rover-photos"></div>
+       <img src="${obj.img_src}" id="rover-photos">
 
         <div id="box-info">
         <p><strong>Camera:</strong> ${obj.camera.full_name}</p>
@@ -26,6 +43,12 @@ fetch("marsrover.json")
 
         </div>`).join("");
 
-        document.querySelector("#container").innerHTML = output;
+        document.querySelector("#display-data").innerHTML = output;
 
     });
+
+
+
+
+
+
