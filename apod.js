74f5a7f48b118e5004795e88a22e80871bf28eb0
mathildahansson <142892122+mathildahansson@users.noreply.github.com>
2024-02-05 15:42:11 +0100

    la till javascript, css samt html

--- apod.js
+++ apod.js
@@ -1,21 +1,36 @@
 const api = "https://api.nasa.gov/planetary/apod?api_key=11dDRgu72MePeDUUGFadfofpAf6Ccc3p45e1ekdf";
 const displayData = document.querySelector("#display-data");
+const form = document.querySelector("#searchForm");
 
-fetch(api)
-.then(response => response.json())
-.then(data => {
-
-    const apodContent = data;
-    console.log(apodContent);
+function handleSearch(event) {
+    event.preventDefault(); // Sidan ska inte laddas on
+    console.log("Allt verkar stämma!");
 
 
-    document.querySelector("#display-data").innerHTML = `
+
+
+    fetch(api)
+        .then(response => response.json())
+        .then(data => {
+            const apodContent = data;
+            console.log(apodContent);
+
+            document.querySelector("#display-data").innerHTML = `
     <h3>${data.title}</h3>
     <img src="${data.hdurl}" alt="APOD" id="apod-img">
     <p><strong>Date:</strong> ${data.date}</p>
     <p><strong>Copyright:</strong> ${data.copyright}</p>
     <details><summary>Explanation</summary>
     <p>${data.explanation}</p></details>`;
+        });
 
-});
 
+
+
+
+    const searchString = document.querySelector("#dateSearch").value;
+    console.log(searchString);
+
+
+
+}
