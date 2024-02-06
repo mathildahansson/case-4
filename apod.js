02488ee565e346a0112c477a04ab7410fb7d5fde
mathildahansson <142892122+mathildahansson@users.noreply.github.com>
2024-02-06 15:19:39 +0100

    strukturerat kod, fixat fel i javascriptkoden, lagt till javascript och ändrat i min css

--- apod.js
+++ apod.js
@@ -3,36 +3,43 @@ var method = "GET";
 
 // API + Endpoints
 const apiKey = "11dDRgu72MePeDUUGFadfofpAf6Ccc3p45e1ekdf";
-var url = "https://api.nasa.gov/planetary/apod?api_key=" + apiKey + "&date=";
+let url = "https://api.nasa.gov/planetary/apod?api_key=" + apiKey;
+
+// Endpoint URL
+let endpoint = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
 
 // DOM Element
-const searchString = document.querySelector("#dateSearch").value;
+// const searchString = document.querySelector("#dateSearch").value;
+const searchString = document.querySelector("#dateInput").value;
 const displayData = document.querySelector("#display-data");
 const form = document.querySelector("#searchForm");
 
 // Formulär-hantering
 function handleSearch(event) {
     event.preventDefault(); // Sidan ska inte laddas on
-    console.log("Allt är OK.");
+    console.log("Is it working?");
+
+    const dateInput = document.querySelector("#dateInput").value;
+    console.log("Valt datum:", dateInput);
+    url += `&date=${dateInput}`;
 
     // Fetch
     fetch(url)
         .then(response => response.json())
         .then(data => {
             const apiData = data;
-            console.log(apiData);
+            console.log(apiData, searchString);
 
             // Visa information på sidan
             document.querySelector("#display-data").innerHTML = `
-    <h3>${data.title}</h3>
+    <h3 id="title">${data.title}</h3>
     <img src="${data.hdurl}" alt="APOD" id="apod-img">
-    <p><strong>Date:</strong> ${data.date}</p>
-    <p><strong>Copyright:</strong> ${data.copyright}</p>
+    <div id="img-info">
+    <p id="info"><strong>Date:</strong> ${data.date}</p>
+    <p id="info"><strong>Copyright:</strong> ${data.copyright}</p>
+    </div>
     <details><summary>Explanation</summary>
     <p>${data.explanation}</p></details>`;
-        });
-
-
-    console.log(fetch(url));
+        })
+        .catch(error => console.log(error));
 }
-
