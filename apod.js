e6c46365b59bb9801860e1129c94c536fc12faf9
mathildahansson <142892122+mathildahansson@users.noreply.github.com>
2024-02-06 10:05:18 +0100

    strukturerat javascript-kod

--- apod.js
+++ apod.js
@@ -1,20 +1,28 @@
-const api = "https://api.nasa.gov/planetary/apod?api_key=11dDRgu72MePeDUUGFadfofpAf6Ccc3p45e1ekdf";
+// Metod
+var method = "GET";
+
+// API + Endpoints
+const apiKey = "11dDRgu72MePeDUUGFadfofpAf6Ccc3p45e1ekdf";
+var url = "https://api.nasa.gov/planetary/apod?api_key=" + apiKey + "&date=";
+
+// DOM Element
+const searchString = document.querySelector("#dateSearch").value;
 const displayData = document.querySelector("#display-data");
 const form = document.querySelector("#searchForm");
 
+// Formulär-hantering
 function handleSearch(event) {
     event.preventDefault(); // Sidan ska inte laddas on
-    console.log("Allt verkar stämma!");
+    console.log("Allt är OK.");
 
-
-
-
-    fetch(api)
+    // Fetch
+    fetch(url)
         .then(response => response.json())
         .then(data => {
-            const apodContent = data;
-            console.log(apodContent);
+            const apiData = data;
+            console.log(apiData);
 
+            // Visa information på sidan
             document.querySelector("#display-data").innerHTML = `
     <h3>${data.title}</h3>
     <img src="${data.hdurl}" alt="APOD" id="apod-img">
@@ -25,12 +33,6 @@ function handleSearch(event) {
         });
 
 
-
-
-
-    const searchString = document.querySelector("#dateSearch").value;
-    console.log(searchString);
-
-
-
+    console.log(fetch(url));
 }
+
